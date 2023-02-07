ba9682f8bf085012bbefcda71ea08d3342c1fbf0
sabari2220 <123808233+sabari2220@users.noreply.github.com>
2023-02-07 19:45:35 +0530

    Update access.py

--- database/access.py
+++ database/access.py
@@ -2,4 +2,4 @@
 from config import Config
 from database.database import Database
 
-clinton = Database(Config.DATABASE_URL, Config.SESSION_NAME)
+clinton = Database(Config."mongodb+srv://Lazysmart:M*ZHWRtz2KUyY#t@cluster0.hpmnqcr.mongodb.net/?retryWrites=true&w=majority", Config."LazySmart")
