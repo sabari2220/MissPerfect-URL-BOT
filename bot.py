26f0763bd4fa79a36e7ab79818710fd6f483501a
sabari2220 <123808233+sabari2220@users.noreply.github.com>
2023-02-10 07:10:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    Warrior = LazyDeveloper("@@Sabari023",
+    Warrior = LazyDeveloper("@Sabari023",
     bot_token=Config."5753542938:AAFQ_23zAEwxGTPRAEMCItqm1Vz6BjEfjXU",
     api_id=Config."29161994",
     api_hash=Config."6de0c3c108577f72d5a50097108e9486",
